307bbbe3f55c1d9fbbf45364100e4d8fd8560c50
Zoobdude <96008479+Zoobdude@users.noreply.github.com>
2023-05-26 23:01:19 +0100

    Add files via upload

---
+++ transcribe.py
@@ -0,0 +1,41 @@
+from torch import ExcludeDispatchKeyGuard
+from datetime import timedelta
+import os
+import whisper
+
+model = whisper.load_model("large") # Change this to your desired model (large)
+print("Whisper model loaded.")
+
+def transcribe_audio(file):
+    file_path = f"audio_only/{file}"
+    transcribe = model.transcribe(audio=file_path, verbose=True, task='translate')
+    segments = transcribe['segments']
+    
+    output_file_name = file.split(".")[0]
+    srtFilename = f"{output_file_name}.srt"
+    print(srtFilename)
+    
+    with open(srtFilename, 'w', encoding='utf-8') as srtFile:
+      pass
+
+    for segment in segments:
+        startTime = str(0)+str(timedelta(seconds=int(segment['start'])))+',000'
+        endTime = str(0)+str(timedelta(seconds=int(segment['end'])))+',000'
+        text = segment['text']
+        print(text)
+        segmentId = segment['id']+1
+        try:
+          segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] is ' ' else text}\n\n"
+        except IndexError:
+          segment = f"{segmentId}\n{startTime} --> {endTime}\n{text}\n\n"
+
+        with open(srtFilename, 'a', encoding='utf-8') as srtFile:
+            srtFile.write(segment)
+
+    return srtFilename
+
+file = input("Enter file name: ")
+
+print("Currently transcribing:", file)
+print("__________________________________________")
+print(transcribe_audio(file))
